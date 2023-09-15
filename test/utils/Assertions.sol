5b903ab87d1c8a043f44cecf2f805fbf93320381
GregTheDev <mapler98@gmail.com>
2023-09-15 18:16:07 -0500

    fix/test-utils

--- test/utils/Assertions.sol
+++ test/utils/Assertions.sol
@@ -1,25 +1,26 @@
-
-//Assert balances are correct after new loan is created
-modifier newLoanAssertions() {
-
-  uint256 borrowerBalance;
-
-  _;
-
-  //Assert updated balances are correct
-
-}
-
-
-//Assert balances are correct after loan is repaid
-modifier repayAssertions() {
-
-  uint256 borrowerBalance;
-
-  _;
-
-  //Assert updated balances are correct
-
+abstract contract Assertions {
+  //Assert balances are correct after new loan is created
+  modifier newLoanAssertions() {
+  
+    uint256 borrowerBalance;
+  
+    _;
+  
+    //Assert updated balances are correct
+  
+  }
+  
+  
+  //Assert balances are correct after loan is repaid
+  modifier repayAssertions() {
+  
+    uint256 borrowerBalance;
+  
+    _;
+  
+    //Assert updated balances are correct
+  
+  }
 }
 
 
