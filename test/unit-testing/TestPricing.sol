52064b8f8ee0edc4683d0afd5df87c9b231027be
justin <1539088+justingreenberg@users.noreply.github.com>
2023-11-02 21:52:06 -0400

    AST-2170 Unit testing pricing module (#42)
    * feat: start unit testing base pricing module
    
    * feat: set default loan duration
    
    * fix: update test
    
    * feat: unit tests for simple interest
    
    * feat: forge fmt
    
    * feat: update snapshot
    
    * feat: update snapshot
    
    * format & snapshot
    
    ---------
    
    Co-authored-by: androolloyd <androolloyd@users.noreply.github.com>

---
+++ test/unit-testing/TestPricing.sol
@@ -0,0 +1,132 @@
+import "forge-std/console2.sol";
+import "starport-test/StarPortTest.sol";
+import {StarPortLib} from "starport-core/lib/StarPortLib.sol";
+import {DeepEq} from "starport-test/utils/DeepEq.sol";
+import {FixedPointMathLib} from "solady/src/utils/FixedPointMathLib.sol";
+import {SpentItemLib} from "seaport-sol/src/lib/SpentItemLib.sol";
+import {Originator} from "starport-core/originators/Originator.sol";
+import {LoanManager} from "starport-core/LoanManager.sol";
+import {ReceivedItem} from "seaport-types/src/lib/ConsiderationStructs.sol";
+
+import {BasePricing} from "starport-core/pricing/BasePricing.sol";
+import {SimpleInterestPricing} from "starport-core/pricing/SimpleInterestPricing.sol";
+
+contract TestSimpleInterestPricing is StarPortTest, DeepEq {
+    using Cast for *;
+    using FixedPointMathLib for uint256;
+
+    LoanManager.Loan public targetLoan;
+
+    function setUp() public override {
+        super.setUp();
+
+        SpentItem[] memory newCollateral = new SpentItem[](1);
+        newCollateral[0] = SpentItem({itemType: ItemType.ERC721, token: address(erc721s[0]), identifier: 1, amount: 1});
+
+        SpentItem[] memory newDebt = new SpentItem[](1);
+        newDebt[0] = SpentItem({itemType: ItemType.ERC20, token: address(erc20s[0]), identifier: 0, amount: 100});
+
+        LoanManager.Loan memory loan = LoanManager.Loan({
+            start: 0,
+            custodian: address(custodian),
+            borrower: borrower.addr,
+            issuer: lender.addr,
+            originator: address(0),
+            collateral: newCollateral,
+            debt: newDebt,
+            terms: LoanManager.Terms({
+                hook: address(hook),
+                handler: address(handler),
+                pricing: address(pricing),
+                pricingData: abi.encode(
+                    BasePricing.Details({carryRate: (uint256(1e16) * 10), rate: (uint256(1e16) * 150) / (365 * 1 days)})
+                    ),
+                handlerData: abi.encode(
+                    DutchAuctionHandler.Details({startingPrice: uint256(500 ether), endingPrice: 100 wei, window: 7 days})
+                    ),
+                hookData: abi.encode(FixedTermHook.Details({loanDuration: 14 days}))
+            })
+        });
+
+        loan.toStorage(targetLoan);
+    }
+
+    function test_getPaymentConsideration() public {
+        SimpleInterestPricing simplePricing = new SimpleInterestPricing(LM);
+
+        SpentItem[] memory repayConsideration;
+        SpentItem[] memory repayCarryConsideration;
+
+        (repayConsideration, repayCarryConsideration) = simplePricing.getPaymentConsideration(targetLoan);
+
+        assertEq(repayConsideration.length, 1);
+        assertEq(repayConsideration[0].token, address(erc20s[0]));
+        assertEq(repayConsideration[0].amount, 100);
+        assertEq(repayConsideration[0].identifier, 0);
+
+        assertEq(repayCarryConsideration.length, 1);
+        assertEq(repayCarryConsideration[0].token, address(erc20s[0]));
+        assertEq(repayCarryConsideration[0].amount, 0);
+        assertEq(repayCarryConsideration[0].identifier, 0);
+
+        // TODO: move to integration tests?
+        vm.warp(60 days);
+
+        (repayConsideration, repayCarryConsideration) = simplePricing.getPaymentConsideration(targetLoan);
+
+        assertEq(repayConsideration.length, 1);
+        assertEq(repayConsideration[0].token, address(erc20s[0]));
+        assertEq(repayConsideration[0].amount, 122);
+        assertEq(repayConsideration[0].identifier, 0);
+
+        assertEq(repayCarryConsideration.length, 1);
+        assertEq(repayCarryConsideration[0].token, address(erc20s[0]));
+        assertEq(repayCarryConsideration[0].amount, 2);
+        assertEq(repayCarryConsideration[0].identifier, 0);
+    }
+
+    function test_calculateInterest() public {
+        SimpleInterestPricing simplePricing = new SimpleInterestPricing(LM);
+
+        uint256 amount = 100;
+        uint256 rate = (uint256(1e16) * 150) / (365 * 1 days);
+        uint256 time = 15 days;
+        uint256 expectedInterest = 6;
+
+        assertEq(simplePricing.calculateInterest(time, amount, rate), expectedInterest);
+
+        // TODO: should this be fuzz tested?
+        assertEq(simplePricing.calculateInterest(time, amount, rate * 2), expectedInterest * 2);
+        assertEq(simplePricing.calculateInterest(time, amount * 2, rate), expectedInterest * 2);
+        assertEq(simplePricing.calculateInterest(time * 2, amount, rate), expectedInterest * 2);
+
+        vm.expectRevert(stdError.arithmeticError);
+        simplePricing.calculateInterest(time - (time * 2), amount, rate);
+
+        vm.expectRevert();
+        simplePricing.calculateInterest(time, amount - (amount * 2), rate);
+
+        vm.expectRevert();
+        simplePricing.calculateInterest(time, amount, rate - (rate * 2));
+    }
+
+    function test_isValidRefinance() public {
+        SimpleInterestPricing simplePricing = new SimpleInterestPricing(LM);
+
+        uint256 baseRate = (uint256(1e16) * 150) / (365 * 1 days);
+
+        simplePricing.isValidRefinance(
+            targetLoan,
+            abi.encode(BasePricing.Details({carryRate: (uint256(1e16) * 10), rate: baseRate / 2})),
+            address(0)
+        );
+
+        vm.expectRevert(bytes4(keccak256("InvalidRefinance()")));
+
+        simplePricing.isValidRefinance(
+            targetLoan,
+            abi.encode(BasePricing.Details({carryRate: (uint256(1e16) * 10), rate: baseRate * 2})),
+            address(0)
+        );
+    }
+}
